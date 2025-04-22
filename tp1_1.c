6b10b030aeb7f0e0c3a9db16bcee1a4d2c0e3c18
Micela Cisneros <cisnerosmica046@gmail.com>
2025-04-22 12:30:38 -0300

    Agrego uso de punteros y sizeof en tp1_1.c

--- tp1_1.c
+++ tp1_1.c
@@ -3,5 +3,15 @@
 int main()
 {
     printf("Hola mundo\n");
+
+    int numero = 10;
+    int *puntero = &numero;
+
+    printf("Contenido del puntero: %d\n", *puntero);
+    printf("Direccion almacenada en el puntero: %p\n", puntero);
+    printf("Direccion de memoria de la variable: %p\n", &numero);
+    printf("Direccion de memoria del puntero: %p\n", &puntero);
+    printf("Tamanio de la variable (bytes): %lu\n", sizeof(numero));
+
     return 0;
 }
